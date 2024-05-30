c09c332ddd387de8f393611cf2d519a2d39a918e
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2024-05-30 23:08:43 +0530

    Optimize & cleaned the codes, improve the imports:

--- WOF-node/src/controllers/userController.js
+++ WOF-node/src/controllers/userController.js
@@ -1,23 +1,23 @@
-const { registerUser, loginUser } = require('../services/UserService');
+const {registerUser, loginUser} = require('../services/UserService');
 
 exports.register = async (req, res) => {
-    const { username, email, password, role } = req.body;
+    const {username, email, password, role} = req.body;
     try {
         const newUser = await registerUser(username, email, password, role);
-        res.status(201).json({ message: 'User registered successfully', user: newUser });
+        res.status(201).json({message: 'User registered successfully', user: newUser});
     } catch (error) {
         console.error('Error registering user:', error.message);
-        res.status(400).json({ message: error.message });
+        res.status(400).json({message: error.message});
     }
 };
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
     try {
         const token = await loginUser(username, password);
-        res.json({ token });
+        res.json({token});
     } catch (error) {
         console.error('Error logging in:', error.message);
-        res.status(401).json({ message: error.message });
+        res.status(401).json({message: error.message});
     }
 };
--- WOF-node/src/middleware/examinerMiddleware.js
+++ WOF-node/src/middleware/examinerMiddleware.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../utils/constants');
+const {JWT_SECRET} = require('../utils/constants');
 
 exports.verifyToken = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     console.log('Received token:', token);
 
     if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
+        return res.status(401).json({message: 'No token provided'});
     }
 
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             console.error('Error verifying token:', err);
-            return res.status(401).json({ message: 'Invalid token' });
+            return res.status(401).json({message: 'Invalid token'});
         }
         console.log('Decoded token:', decoded);
 
         // Check if the decoded token contains necessary information for vehicle authentication
         if (!decoded || !decoded.role || decoded.role !== 'Examiner') {
-            return res.status(403).json({ message: 'Unauthorized' });
+            return res.status(403).json({message: 'Unauthorized'});
         }
         req.user = decoded;
         next();
