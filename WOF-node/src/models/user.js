0b0015a072f069d0af21b8c4dc1c5597898fe8d9
Lakshitha Geethmal <lakshithageethmal@gmail.com>
2024-10-11 22:23:58 +0530

    update user model and attributes

--- WOF-node/src/models/user.js
+++ WOF-node/src/models/user.js
@@ -4,7 +4,11 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, unique: true, sparse: true },
-    role: { type: String, default: 'user' }
+    role: { type: String, default: 'user' },
+    firstName: { type: String, required: false },
+    lastName: { type: String, required: false },
+    profilePicture: { type: String, required: false },
+    dateOfBirth: { type: Date }
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
